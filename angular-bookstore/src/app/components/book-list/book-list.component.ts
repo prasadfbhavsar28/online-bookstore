355ebf76b911d8b02bf4303b97db37fbaca3444e
Prasad Bhavsar <prasad.fbhavsar28@gmail.com>
2020-04-30 22:01:31 +0530

    Created angular project and completed the feature 1.

---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[] = [];
+
+  constructor(private _bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.listBooks();
+  }
+
+  listBooks() {
+    this._bookService.getAllBooks().subscribe(
+      data => this.books = data
+    )
+  }
+}
