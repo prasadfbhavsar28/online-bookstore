b91aadc9cac592d91b4395b0cad18583ec504845
Prasad Bhavsar <prasad.fbhavsar28@gmail.com>
2020-05-01 18:18:16 +0530

    Completed feature 4 dynamic categories

--- onlinebookstore/src/main/java/com/prasad/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/com/prasad/onlinebookstore/config/RepositoryConfig.java
@@ -20,6 +20,10 @@ public class RepositoryConfig implements RepositoryRestConfigurer {
 		config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
 				.map(Type::getJavaType)
 				.toArray(Class[]::new));
+		
+		config.getCorsRegistry()
+			.addMapping("/**")
+			.allowedOrigins("http://localhost:4200");
 	}
 
 }
--- onlinebookstore/src/main/java/com/prasad/onlinebookstore/repository/BookCategoryRepository.java
+++ onlinebookstore/src/main/java/com/prasad/onlinebookstore/repository/BookCategoryRepository.java
@@ -2,7 +2,6 @@ package com.prasad.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import com.prasad.onlinebookstore.entity.BookCategory;
 
 @RepositoryRestResource(collectionResourceRel = "bookCategory", path = "book-category")
